c2aaf75ec82992541ccdd1b029a07f82c7da6dc8
miyazakid <iamfintanmaher@gmail.com>
2019-10-12 17:27:45 -0400

    Add major nav

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './App.css';
 import Aux from './hoc/Aux/Aux';
 import MinorNavbar from './components/Navigation/MinorNav/MinorNav';
+import MajorNav from './components/Navigation/MajorNav/MajorNav';
 
 function App() {
   return (
     <Aux>
       <MinorNavbar />
-
+      <MajorNav />
     </Aux>
   );
 }
--- src/components/Navigation/MajorNav/MajorNav.js
+++ src/components/Navigation/MajorNav/MajorNav.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MajorNav extends Component {
+import classes from './MajorNav.module.css';
 
-    render () {
+const MajorNav = (props) => (
+  <div className={classes.MajorNav}>
 
-    }
-}
+    <input type="text" placeholder="Search..." />
+
+    <input type="text" placeholder="Testing fonts here..." />
+
+    <select>
+     <option value="12px">12px</option>
+     <option value="18px">18px</option>
+     <option value="24px">24px</option>
+     <option value="32px">32px</option>
+    </select>
+
+    <div>
+      <input type="radio" name="colormode" value="dark" />
+      <input type="radio" name="colormode" value="light" />
+    </div>
+
+    <button>Grid/List mode</button>
+
+    <button>Reset</button>
+  </div>
+);
 
 export default MajorNav;
